735295a02f1d11705e4aa74939d561e8d22740ad
lyre <lyre@poetpalace.org>
2020-10-05 16:13:16 +0800

    bfs test

--- bintree/bintree_test.go
+++ bintree/bintree_test.go
@@ -1,10 +1,19 @@
 package bintree
 
 import (
-	"github.com/davecgh/go-spew/spew"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
+func TestNewFromSliceEmpty(t *testing.T) {
+	is := []interface{}{}
+	bt := NewFromSlice(is)
+
+	if bt != nil {
+		t.Errorf("tree shoule be nil\n")
+	}
+}
 func TestNewFromSlice(t *testing.T) {
 	is := []interface{}{3, 9, 20, nil, nil, 15, 7}
 	bt := NewFromSlice(is)
